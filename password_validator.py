c2227ae69cb9a00f8326727d7a0825a4c4bb1b1d
wahdanEw <85776760+wahdanEw@users.noreply.github.com>
2022-07-15 22:13:51 +0300

    added password validator script

---
+++ password_validator.py
@@ -0,0 +1,32 @@
+import re
+from termcolor import colored
+import sys
+
+
+def validate_password(passwd) -> str:
+    """
+    an example: validate_password(my password) -> str(user message valid/invalid)
+    :param passwd: str
+    """
+    if len(passwd) >= 10:
+        if(bool(re.match('((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{10,30})',passwd))==True):
+            print(colored("your password is strong", "green"))
+        else:
+            print(colored("your password is weak", "red"))
+    else:
+        print(colored("You have entered an invalid password, "
+                      "password should contain:\n"
+                      "length minimum of 10 characters\n"
+                      "alphabet and number, small and capital case letters", "red"))
+
+
+def main():
+    if len(sys.argv) == 2:
+        validate_password(sys.argv[1])
+    else:
+        print(colored("Wrong input, an example for valid input:\n"
+                      "python ./password_validator.py \"MyP@ssw0rd!\"\n", "red"))
+
+
+if __name__ == '__main__':
+    main()
